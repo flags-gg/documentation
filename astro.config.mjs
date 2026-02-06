4dd316866efef0bf7f8e0df71ebcbb3facabca85
Keloran <keloran@develbox.info>
2026-02-06 10:39:31 +0000

    see if this works

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ export default defineConfig({
   site: "https://docs.flags.gg",
   output: "server",
   adapter: node({ mode: "standalone" }),
+  server: { host: "0.0.0.0" },
   integrations: [
     starlight({
       title: "Flags.gg",
